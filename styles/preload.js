7509908a3e47173515dc8d21866e614500028dcb
Saleh <saleh@grasppe.com>
2019-03-21 13:59:05 -0400

    Cleanup stray console.log in styles/preload.js

--- styles/preload.js
+++ styles/preload.js
@@ -1,16 +1,17 @@
 ﻿if (typeof document === 'object' && document.currentScript) {
-  const {currentScript, links} = document;
+	const {currentScript, links} = document;
 
-  const snaps = {};
-  const counter = (target, first = 'a', field = 'index') => String.fromCharCode((target[field] == null && (target[field] = first.charCodeAt(0)), target[field]++));
+	const snaps = false; // {};
+	const counter = (target, first = 'a', field = 'index') =>
+		String.fromCharCode((target[field] == null && (target[field] = first.charCodeAt(0)), target[field]++));
 
-  // const snap = (id = String.fromCharCode((snap.index || (snap.index = 'a'.charCodeAt(0)), snap.index++))) => console.log.bind(null, '%O ‹%s› — %o', currentScript, id, (snaps[id] = {links: [... links]}));
-  const snap = (id = counter(snap)) => console.log.bind(null, '%O ‹%s› — %o', currentScript, id, (snaps[id] = {links: [... links]}));
+	const snap = (id = counter(snap)) =>
+		console.log.bind(null, '%O ‹%s› — %o', currentScript, id, (snaps[id] = {links: [...links]}));
 
-  snap()();
+	snaps && snap()();
 
-  const template = document.createElement('template');
-  template.innerHTML = `
+	const template = document.createElement('template');
+	template.innerHTML = `
 		<link rel="stylesheet" href="/pholio/styles/fonts/iosevka/iosevka.css" />
     <link rel="stylesheet" href="/pholio/styles/styles.css" />
     <!-- <link rel="preload" as="style" href="/pholio/styles/styles.css" /> -->
@@ -19,15 +20,15 @@
 		<link rel="preload" as="style" href="/pholio/styles/common.css" />
   `;
 
-  snap()();
+	snaps && snap()();
 
-  currentScript.after(template.content.cloneNode(true));
+	currentScript.after(template.content.cloneNode(true));
 
-  snap()();
+	snaps && snap()();
 
-  requestAnimationFrame(() => {
-    snap()();
-    currentScript.after(template.clone);
-    snap()();
-  });
+	requestAnimationFrame(() => {
+		snaps && snap()();
+		currentScript.after(template.clone);
+		snaps && snap()();
+	});
 }
