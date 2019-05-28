9ca8bd22398d3f8d39a11b183de0abbe026f484a
Saleh <saleh@grasppe.com>
2019-05-28 13:01:39 -0400

    Add `declarativeStyling.autoprefix(‹string:CSSValue›): string`

--- elements/markout-content.js
+++ elements/markout-content.js
@@ -356,15 +356,24 @@ try {
 			selector: '',
 			apply: element => {
 				const style = element.style;
-				const lookup = declarativeStyling.lookup;
-				for (const attribute of element.getAttributeNames())
-					attribute in lookup &&
-						((style[lookup[attribute]] = element.getAttribute(attribute)), element.removeAttribute(attribute));
+				const {lookup, autoprefix} = declarativeStyling;
+				if (autoprefix) {
+					for (const attribute of element.getAttributeNames())
+						attribute in lookup &&
+							((style[lookup[attribute]] = autoprefix(element.getAttribute(attribute))),
+							element.removeAttribute(attribute));
+				} else {
+					for (const attribute of element.getAttributeNames())
+						attribute in lookup &&
+							((style[lookup[attribute]] = element.getAttribute(attribute)), element.removeAttribute(attribute));
+				}
 			},
+			/** @type {(value: string) => string} */
+			autoprefix: undefined,
 		};
 
 		Initialization: {
-			const {getOwnPropertyNames, setPrototypeOf, getPrototypeOf, freeze} = Object;
+			const {getOwnPropertyNames, setPrototypeOf, getPrototypeOf, freeze, keys} = Object;
 			const {lookup} = declarativeStyling;
 			const Filter = /^(?!webkit[A-Z])(?!moz[A-Z])[a-zA-Z]{2,}$/;
 			const Boundary = /[a-z](?=[A-Z])/g;
@@ -386,8 +395,45 @@ try {
 
 			declarativeStyling.selector = selectors.join(',');
 			freeze(setPrototypeOf(declarativeStyling.lookup, null));
-			freeze(declarativeStyling);
 			freeze(declarativeStyling.apply);
+
+			Prefixes: {
+				const autoprefix = value => {
+					const prefixed = value.replace(autoprefix.matcher, autoprefix.replacer);
+					console.log(value, prefixed);
+					return prefixed;
+				};
+				autoprefix.mappings = {};
+				autoprefix.prefix = CSS.supports('-moz-appearance', 'initial')
+					? '-moz-'
+					: CSS.supports('-webkit-appearance', 'initial')
+					? '-webkit-'
+					: '';
+
+				if (autoprefix.prefix) {
+					const {mappings, prefix} = autoprefix;
+					const map = (property, value, mapping = `${prefix}${value}`) =>
+						CSS.supports(property, value) || (mappings[value] = mapping);
+
+					if (prefix === '-webkit-') {
+						map('width', 'fill-available');
+					} else if (prefix === '-moz-') {
+						map('width', 'fill-available', '-moz-available');
+					}
+
+					const mapped = keys(mappings);
+
+					if (mapped.length > 0) {
+						autoprefix.matcher = new RegExp(String.raw`\b-?(?:${mapped.join('|')})\b`, 'gi');
+						freeze((autoprefix.replacer = value => mappings[value] || value));
+						freeze(autoprefix.mappings);
+						freeze((declarativeStyling.autoprefix = autoprefix));
+					}
+					// console.log(autoprefix, {...autoprefix});
+				}
+			}
+
+			freeze(declarativeStyling);
 		}
 
 		return declarativeStyling;
