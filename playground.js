0858329fec0d74ca41b85e8d434bcbdf29712849
Saleh Abdel Motaal <saleh@grasppe.com>
2019-10-18 13:39:49 -0400

    Fix playgrounds crashing on legacy — ie `globalThis`

--- playground.js
+++ playground.js
@@ -1,36 +1,42 @@
-﻿if (/^about:/.test(globalThis.location)) {
-	import('/browser/markout-frame.js?resize');
+﻿if (typeof self === 'object' && self && self === self.self) {
+	(typeof globalThis === 'object' && globalThis) || (1, eval)('this.globalThis = this');
 
-	const inDevelopment = /\?.*?\bdev\b/i.test(import.meta.url);
-	const markoutURL = inDevelopment ? './lib/browser.js' : './dist/browser.js';
-	const stylesURL = '/markout/styles/styles.css';
-	const rootStylesURL = '/markout/styles/root.css';
-	const contentStyleID = 'style:styles/markout.css';
-	const contentStyleURL = '/markout/styles/markout.css';
+	if (/^about:/.test(self.location)) {
+		import('/browser/markout-frame.js?resize');
 
-	// Content Styles
-	document.head.querySelector(`link[id="${contentStyleID}"][rel=preload][as=style]`) ||
-		document.head.appendChild(
-			Object.assign(document.createElement('link'), {
-				id: contentStyleID,
-				rel: 'preload',
-				as: 'style',
-				href: contentStyleURL,
-			}),
-		);
+		const inDevelopment = /\?.*?\bdev\b/i.test(import.meta.url);
+		const markoutURL = inDevelopment ? './lib/browser.js' : './dist/browser.js';
+		const stylesURL = '/markout/styles/styles.css';
+		const rootStylesURL = '/markout/styles/root.css';
+		const contentStyleID = 'style:styles/markout.css';
+		const contentStyleURL = '/markout/styles/markout.css';
 
-	// Root Styles
-	document.head.querySelector(
-		`link[rel=stylesheet][href*="${stylesURL}"],link[rel=stylesheet][href*="${rootStylesURL}"]`,
-	) ||
-		document.head.appendChild(
-			Object.assign(document.createElement('link'), {
-				rel: 'stylesheet',
-				href: rootStylesURL,
-			}),
-		);
+		// Content Styles
+		document.head.querySelector(`link[id="${contentStyleID}"][rel=preload][as=style]`) ||
+			document.head.appendChild(
+				Object.assign(document.createElement('link'), {
+					id: contentStyleID,
+					rel: 'preload',
+					as: 'style',
+					href: contentStyleURL,
+				}),
+			);
 
-	import(markoutURL);
-} else if (typeof globalThis.customElements === 'object') {
-	customElements.get('markout-playground') || import('./elements/markout-playground.js');
+		// Root Styles
+		document.head.querySelector(
+			`link[rel=stylesheet][href*="${stylesURL}"],link[rel=stylesheet][href*="${rootStylesURL}"]`,
+		) ||
+			document.head.appendChild(
+				Object.assign(document.createElement('link'), {
+					rel: 'stylesheet',
+					href: rootStylesURL,
+				}),
+			);
+
+		import(markoutURL);
+	} else if (typeof self.customElements === 'object') {
+		customElements.get('markout-playground') || import('./elements/markout-playground.js');
+	}
+} else {
+	console.warn('Unsupported Environment');
 }
